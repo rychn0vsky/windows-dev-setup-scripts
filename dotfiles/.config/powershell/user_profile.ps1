06cf663af5888e24023b5f97c41cf5b88c191633
rychn0vsky <98218868+rychn0vsky@users.noreply.github.com>
2023-03-09 09:24:17 +0100

    Added dofiles with powershell user profile config

---
+++ dotfiles/.config/powershell/user_profile.ps1
@@ -0,0 +1,28 @@
+# Prompt
+$omp_config = Join-Path $PSScriptRoot ".\paradox.omp.json"
+oh-my-posh init pwsh --config $omp_config | Invoke-Expression
+
+# Terminal-Icons
+Import-Module Terminal-Icons
+
+# Posh-git
+Import-Module posh-git
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Windows
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias cat type
+Set-Alias grep findstr
+
+# Which command
+function which ($command) { 
+  Get-Command -Name $command -ErrorAction SilentlyContinue | 
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue 
+} 
